515904a884f38eda7de7ed200e2ae8dd0d9ff9e2
Nico <nic.girault@gmail.com>
2015-05-30 11:25:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Simple Gitbook plugin to include js in pages
+==============
+
+Install
+-------
+
+In the book.json file, write
+
+```
+{
+    "plugins": ["colorbrewer-picker"],
+    "pluginsConfig": {
+        "colorbrewer-picker": {}
+    }
+}
+```
+
+Then:
+
+```
+gitbook install
+```
+
---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Extend website resources and html
+    website: {
+        html: {
+            "head:end": "<script src='https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min.js'></script>\n<script src='https://raw.githack.com/nicgirault/angular-colorBrewer-picker/master/dist/angular-color-brewer-picker.min.js'></script>\n<link rel='stylesheet' href='https://cdn.rawgit.com/nicgirault/angular-colorBrewer-picker/master/dist/angular-color-brewer-picker.min.css'></link>"
+        }
+    }
+};
+
